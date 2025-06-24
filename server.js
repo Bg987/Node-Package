957f6dfd0c9ee917670637af223f254dfd50be06
Bg <119032764+Bg987@users.noreply.github.com>
2025-06-24 19:03:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/', (req, res) => {
         ]
     });
 })
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: 'Not found'
+  });
+});
 app.listen(port, () => {
     console.log(`Server is running`);
 });
